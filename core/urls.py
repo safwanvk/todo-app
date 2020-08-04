42b891251d1ab05bb013c789489b0362f8c76d1c
safwanvk <safwanvalakundil@gmail.com>
2020-08-04 17:17:43 +0530

    :white_check_mark: Set task_detail

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.api_overview, name='api-overview'),
-    path('task-list/', views.tasklist, name='task-list')
+    path('task-list/', views.task_list, name='task-list'),
+    path('task-detail/<str:pk>/', views.task_detail, name='task-detail')
 ]
--- core/views.py
+++ core/views.py
@@ -18,8 +18,15 @@ def api_overview(request):
     return Response(api_urls)
 
 @api_view(['GET'])
-def tasklist(request):
+def task_list(request):
     tasks = Task.objects.all()
     serializer = TaskSerializer(tasks, many=True)
+    
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def task_detail(request, pk):
+    tasks = Task.objects.get(id=pk)
+    serializer = TaskSerializer(tasks, many=False)
     
     return Response(serializer.data)
