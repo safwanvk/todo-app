92a657fb884def20c7c9b25a059ebee1b754de52
safwanvk <safwanvalakundil@gmail.com>
2020-08-04 16:02:06 +0530

    :zap: Set base api_overvew

--- core/views.py
+++ core/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+def api_overview(request):
+    return JsonResponse('Api Base Point', safe=False)
