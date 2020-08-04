b92187c836f0f62796af7f8f6703fe187e5c1113
safwanvk <safwanvalakundil@gmail.com>
2020-08-04 16:48:15 +0530

    :white_check_mark: Set api_overview

--- core/views.py
+++ core/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+@api_view(['GET'])
 def api_overview(request):
-    return JsonResponse('Api Base Point', safe=False)
+    api_urls = {
+        'list': '/task/list/',
+        'detail-view': '/task-detail/<str:pk>/',
+        'create': '/task-create',
+        'update': '/task-update/<str:pk>/',
+        'delete': '/task-delete/<str:pk>/'
+    }
+    return Response(api_urls)
