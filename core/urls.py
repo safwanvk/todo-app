4ca95395ce0eadf4d7d6a1c3103d888ae370eb29
safwanvk <safwanvalakundil@gmail.com>
2020-08-04 17:12:31 +0530

    :white_check_mark: Set tasklist

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.api_overview, name='api_overview')
+    path('', views.api_overview, name='api-overview'),
+    path('task-list/', views.tasklist, name='task-list')
 ]
--- core/views.py
+++ core/views.py
@@ -2,15 +2,24 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import TaskSerializer
+from .models import Task
 
 # Create your views here.
 @api_view(['GET'])
 def api_overview(request):
     api_urls = {
-        'list': '/task/list/',
+        'list': '/task-list/',
         'detail-view': '/task-detail/<str:pk>/',
-        'create': '/task-create',
+        'create': '/task-create/',
         'update': '/task-update/<str:pk>/',
         'delete': '/task-delete/<str:pk>/'
     }
     return Response(api_urls)
+
+@api_view(['GET'])
+def tasklist(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    
+    return Response(serializer.data)
